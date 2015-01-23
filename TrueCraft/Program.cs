ff00c0bfc01645368b2b227e4f016ea3e9d215ac
Daniel Vidmar <daniel.viddy@gmail.com>
2015-01-23 13:17:14 -0500

    Moved the command check back to Program.cs

--- TrueCraft/Program.cs
+++ TrueCraft/Program.cs
@@ -16,10 +16,11 @@ namespace TrueCraft
     class MainClass
     {
         public static CommandManager CManager;
+        private static MultiplayerServer server;
         public static void Main(string[] args)
         {
             // TODO: Make this more flexible
-            var server = new MultiplayerServer();
+            server = new MultiplayerServer();
             server.AddWorld(new World("default", new StandardGenerator()));
             server.AddLogProvider(new ConsoleLogProvider());
             #if DEBUG
@@ -36,6 +37,22 @@ namespace TrueCraft
         static void HandleChatMessageReceived(object sender, ChatMessageEventArgs e)
         {
             // TODO: Make this more sophisticated
+            if (e.Message.StartsWith("/"))
+            {
+                e.PreventDefault = true;
+                var Message = e.Message.Remove(0, 1);
+                var Command = Message.Trim();
+                var Arguments = new string[0];
+                if (Message.Split(' ').Length > 1)
+                {
+                    Command = Message.Split(' ')[0];
+                    Arguments = Message.Substring(Command.Length).Trim().Split(' ');
+                }
+
+                var cmdArgs = new CommandEventArgs(e.Client, Command, Arguments);
+                server.OnCommandReceived(cmdArgs);
+                return;
+            }
         }
 
         static void HandleCommandReceived(object sender, CommandEventArgs e)
