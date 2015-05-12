9ab181fab101591852424e2944180497fe64dcd4
Drew DeVault <sir@cmpwn.com>
2015-05-12 17:39:34 -0600

    Reduce window to 720p and disable client in DEBUG
    This also disables it in RELEASE. If you want to build the client, then
    you'll want to build the Client target.

---
+++ TrueCraft.Client.Linux/Interface/IGameInterface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace TrueCraft.Client.Linux.Interface
+{
+    public interface IGameInterface
+    {
+        void Update(GameTime gameTime);
+        void Draw(GameTime gameTime);
+    }
+}
--- TrueCraft.Client.Linux/TrueCraftGame.cs
+++ TrueCraft.Client.Linux/TrueCraftGame.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace TrueCraft.Client.Linux
 {
@@ -8,15 +9,20 @@ namespace TrueCraft.Client.Linux
     {
         private MultiplayerClient Client { get; set; }
         private GraphicsDeviceManager Graphics { get; set; }
-        
+
         public TrueCraftGame(MultiplayerClient client)
         {
             Window.Title = "TrueCraft";
             Graphics = new GraphicsDeviceManager(this);
             Graphics.IsFullScreen = false;
-            Graphics.PreferredBackBufferWidth = 1920;
-            Graphics.PreferredBackBufferHeight = 1080;
+            Graphics.PreferredBackBufferWidth = 1280;
+            Graphics.PreferredBackBufferHeight = 720;
             Client = client;
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
         }
 
         protected override void Update(GameTime gameTime)
