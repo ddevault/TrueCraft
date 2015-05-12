47733bb5fb39c5c92ee9aa8488d344a9803dcb40
Drew DeVault <sir@cmpwn.com>
2015-05-12 16:48:58 -0600

    Create TrueCraft.Client.Linux
    At some point I'll work on other platforms.

---
+++ TrueCraft.Client.Linux/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TrueCraft.Client.Linux
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var game = new TrueCraftGame();
+            game.Run();
+        }
+    }
+}
---
+++ TrueCraft.Client.Linux/TrueCraftGame.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace TrueCraft.Client.Linux
+{
+    public class TrueCraftGame : Game
+    {
+        private GraphicsDeviceManager Graphics { get; set; }
+        
+        public TrueCraftGame()
+        {
+            Window.Title = "TrueCraft";
+            Graphics = new GraphicsDeviceManager(this);
+            Graphics.IsFullScreen = false;
+            Graphics.PreferredBackBufferWidth = 1920;
+            Graphics.PreferredBackBufferHeight = 1080;
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            Graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+            base.Draw(gameTime);
+        }
+    }
+}
