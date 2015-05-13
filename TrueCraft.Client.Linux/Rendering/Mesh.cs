0149af3ba245f14d19cddd24ab1152e364475ef6
Drew DeVault <sir@cmpwn.com>
2015-05-13 17:02:03 -0600

    Add Mesh class

---
+++ TrueCraft.Client.Linux/Rendering/Mesh.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TrueCraft.Client.Linux.Rendering
+{
+    public class Mesh
+    {
+        public VertexBuffer Verticies { get; set; }
+        public IndexBuffer Indicies { get; set; }
+
+        public Mesh(GraphicsDevice device, VertexPositionNormalTexture[] verticies, int[] indicies)
+        {
+            Verticies = new VertexBuffer(device, VertexPositionNormalTexture.VertexDeclaration,
+                verticies.Length, BufferUsage.None);
+            Verticies.SetData(verticies);
+            Indicies = new IndexBuffer(device, IndexElementSize.ThirtyTwoBits, indicies.Length, BufferUsage.None);
+            Indicies.SetData(indicies);
+        }
+
+        ~Mesh()
+        {
+            if (Verticies != null)
+                Verticies.Dispose();
+            if (Indicies != null)
+                Indicies.Dispose();
+        }
+
+        public void Draw(Effect effect)
+        {
+            effect.GraphicsDevice.SetVertexBuffer(Verticies);
+            effect.GraphicsDevice.Indices = Indicies;
+            foreach (var pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                effect.GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList,
+                    0, 0, Indicies.IndexCount, 0, Indicies.IndexCount / 3);
+            }
+        }
+    }
+}
