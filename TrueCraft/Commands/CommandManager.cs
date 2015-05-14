f9900f6d358abc01984b58f9f383c4871f7fb147
Robin Kanters <robin@robinkanters.nl>
2015-05-14 13:08:05 +0200

    Cleaned code in the files I changed

--- TrueCraft/Commands/CommandManager.cs
+++ TrueCraft/Commands/CommandManager.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using TrueCraft.API.Server;
 using TrueCraft.API.Networking;
+using TrueCraft.API.Server;
 
 namespace TrueCraft.Commands
 {
