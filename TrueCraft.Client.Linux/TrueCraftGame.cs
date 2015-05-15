fc5e1aaa2f496d02e20c9ff080801aba51a768b4
Drew DeVault <sir@cmpwn.com>
2015-05-15 15:50:19 -0600

    Fix issue with on-screen metrics

--- TrueCraft.Client.Linux/TrueCraftGame.cs
+++ TrueCraft.Client.Linux/TrueCraftGame.cs
@@ -237,7 +237,7 @@ namespace TrueCraft.Client.Linux
             DejaVu.DrawText(SpriteBatch, 0, 500, string.Format("X: {0}, Y: {1}, Z: {2}", Client.Position.X, Client.Position.Y, Client.Position.Z));
             DejaVu.DrawText(SpriteBatch, 0, 530, string.Format("Yaw: {0}, Pitch: {1}", Client.Yaw, Client.Pitch));
             DejaVu.DrawText(SpriteBatch, 0, GraphicsDevice.Viewport.Height - 30,
-                string.Format("{0} FPS, {1} verticies, {1} chunks", fps, verticies, chunks));
+                string.Format("{0} FPS, {1} verticies, {2} chunks", fps + 1, verticies, chunks));
             SpriteBatch.End();
             base.Draw(gameTime);
         }
