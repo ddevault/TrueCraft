93e729333c803b5cfd2685d50f5156de351f327d
Robin Kanters <robin@robinkanters.nl>
2015-05-16 12:07:39 +0200

    Herp derp. AccessConfiguration shouldn't use config.yml

--- TrueCraft/Program.cs
+++ TrueCraft/Program.cs
@@ -110,13 +110,13 @@ namespace TrueCraft
             if (File.Exists(configFileName))
             {
                 var deserializer = new Deserializer(ignoreUnmatched: true);
-                using (var file = File.OpenText("config.yaml"))
+                using (var file = File.OpenText(configFileName))
                     config = deserializer.Deserialize<T>(file);
             }
             else
                 config = new T();
             var serializer = new Serializer();
-            using (var writer = new StreamWriter("config.yaml"))
+            using (var writer = new StreamWriter(configFileName))
                 serializer.Serialize(writer, config);
 
             return config;
