626c2bbba1c1cd4b9293adfde82d81c0a2389b0b
Robin Kanters <robin@robinkanters.nl>
2015-05-23 14:54:38 +0200

    Now showing help page #1 by default if no page number is given

--- TrueCraft/Commands/HelpCommand.cs
+++ TrueCraft/Commands/HelpCommand.cs
@@ -18,13 +18,13 @@ namespace TrueCraft.Commands
 
         public override void Handle(IRemoteClient client, string alias, string[] arguments)
         {
-            if (arguments.Length < 1)
+            if (arguments.Length > 1)
             {
                 Help(client, alias, arguments);
                 return;
             }
 
-            var identifier = arguments.Length >= 1 ? arguments[0] : "0";
+            var identifier = arguments.Length == 1 ? arguments[0] : "1";
 
             ICommand found;
             if ((found = Program.CommandManager.FindByName(identifier)) != null)
