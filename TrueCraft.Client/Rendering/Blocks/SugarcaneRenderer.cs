a8307ae28ee576159941d767e7783e75cb6e0aec
Drew DeVault <sir@cmpwn.com>
2015-06-03 21:36:21 -0600

    Add sugar cane rendering

---
+++ TrueCraft.Client/Rendering/Blocks/SugarcaneRenderer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TrueCraft.Core.Logic.Blocks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using TrueCraft.API.Logic;
+
+namespace TrueCraft.Client.Rendering.Blocks
+{
+    public class SugarcaneRenderer : FlatQuadRenderer
+    {
+        static SugarcaneRenderer()
+        {
+            BlockRenderer.RegisterRenderer(SugarcaneBlock.BlockID, new SugarcaneRenderer());
+        }
+
+        protected override Vector2 TextureMap { get { return new Vector2(9, 4); } }
+    }
+}
