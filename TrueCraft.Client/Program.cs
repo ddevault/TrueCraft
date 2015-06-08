adbe5010f944f997297a29ce81d945b8026c5f61
Robin Kanters <robin@robinkanters.nl>
2015-06-09 00:02:44 +0200

    Moved endpoint parsing to Core so the launcher can use it, too

--- TrueCraft.Client/Program.cs
+++ TrueCraft.Client/Program.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Linq;
 using System.Net.Sockets;
 using TrueCraft.Core;
+using TrueCraft.Core.Util;
 
 namespace TrueCraft.Client
 {
@@ -13,33 +14,9 @@ namespace TrueCraft.Client
         {
             var user = new TrueCraftUser { Username = args[1] };
             var client = new MultiplayerClient(user);
-            var game = new TrueCraftGame(client, ParseEndPoint(args[0]));
+            var game = new TrueCraftGame(client, EndPointParser.ParseEndPoint(args[0]));
             game.Run();
             client.Disconnect();
-        }
-
-        private static IPEndPoint ParseEndPoint(string arg)
-        {
-            IPAddress address;
-            int port;
-            if (arg.Contains(':'))
-            {
-                // Both IP and port are specified
-                var parts = arg.Split(':');
-                if (!IPAddress.TryParse(parts[0], out address))
-                    address = Resolve(parts[0]);
-                return new IPEndPoint(address, int.Parse(parts[1]));
-            }
-            if (IPAddress.TryParse(arg, out address))
-                return new IPEndPoint(address, 25565);
-            if (int.TryParse(arg, out port))
-                return new IPEndPoint(IPAddress.Loopback, port);
-            return new IPEndPoint(Resolve(arg), 25565);
-        }
-
-        private static IPAddress Resolve(string arg)
-        {
-            return Dns.GetHostEntry(arg).AddressList.FirstOrDefault(item => item.AddressFamily == AddressFamily.InterNetwork);
         }
     }
 }
