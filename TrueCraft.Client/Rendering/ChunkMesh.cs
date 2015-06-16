3272056b8fa41d3d49823d5165dccde69903a9f1
William Moorehouse <moorehousew@gmail.com>
2015-06-16 15:53:28 -0400

    Refactored and expanded Mesh

--- TrueCraft.Client/Rendering/ChunkMesh.cs
+++ TrueCraft.Client/Rendering/ChunkMesh.cs
@@ -5,19 +5,43 @@ using Microsoft.Xna.Framework;
 
 namespace TrueCraft.Client.Rendering
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class ChunkMesh : Mesh
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public ReadOnlyChunk Chunk { get; set; }
 
-        public ChunkMesh(ReadOnlyChunk chunk, GraphicsDevice device, VertexPositionNormalTexture[] verticies, int[] indicies)
-            : base(device, verticies, indicies, false)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="chunk"></param>
+        /// <param name="device"></param>
+        /// <param name="vertices"></param>
+        /// <param name="indices"></param>
+        public ChunkMesh(ReadOnlyChunk chunk, GraphicsDevice device, VertexPositionNormalTexture[] vertices, int[] indices)
+            : base(device, 1, true)
         {
             Chunk = chunk;
-            BoundingBox = new BoundingBox(
-                new Vector3(chunk.X * TrueCraft.Core.World.Chunk.Width, 0, chunk.Z * TrueCraft.Core.World.Chunk.Depth),
-                new Vector3(chunk.X * TrueCraft.Core.World.Chunk.Width
+            Vertices = vertices;
+            SetSubmesh(0, indices);
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="vertices"></param>
+        /// <returns></returns>
+        protected override BoundingBox RecalculateBounds(VertexPositionNormalTexture[] vertices)
+        {
+            return new BoundingBox(
+                new Vector3(Chunk.X * TrueCraft.Core.World.Chunk.Width, 0, Chunk.Z * TrueCraft.Core.World.Chunk.Depth),
+                new Vector3(Chunk.X * TrueCraft.Core.World.Chunk.Width
                     + TrueCraft.Core.World.Chunk.Width, TrueCraft.Core.World.Chunk.Height,
-                    chunk.Z * TrueCraft.Core.World.Chunk.Depth + TrueCraft.Core.World.Chunk.Depth));
+                    Chunk.Z * TrueCraft.Core.World.Chunk.Depth + TrueCraft.Core.World.Chunk.Depth));
         }
     }
 }
