ce4d662e67c80a71f5ca15ba3147d240e31f0ce7
Drew DeVault <sir@cmpwn.com>
2015-06-18 12:53:24 -0400

    Replace workaround with better solution
    Thanks @mlehmk

--- TrueCraft.Client/Program.cs
+++ TrueCraft.Client/Program.cs
@@ -10,14 +10,14 @@ namespace TrueCraft.Client
 {
     public static class Program
     {
+        [STAThread]
         public static void Main(string[] args)
         {
             AppDomain.CurrentDomain.AssemblyResolve += AppDomain_CurrentDomain_AssemblyResolve;
 
-            var thread = new Thread(() => Main_Thread(args));
-            thread.SetApartmentState(ApartmentState.STA);
-            thread.Start();
-            thread.Join();
+            // We need to run this in another method to avoid referencing MonoGame before registering
+            // our AssemblyResolve handler
+            Main_Thread(args);
         }
 
         static Assembly AppDomain_CurrentDomain_AssemblyResolve (object sender, ResolveEventArgs args)
