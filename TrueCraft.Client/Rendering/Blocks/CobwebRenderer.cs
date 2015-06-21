2da59178f01eb52660450d5cba54322f536c07c6
William Moorehouse <moorehousew@gmail.com>
2015-06-21 18:36:02 -0400

    Simplified cobweb renderer

--- TrueCraft.Client/Rendering/Blocks/CobwebRenderer.cs
+++ TrueCraft.Client/Rendering/Blocks/CobwebRenderer.cs
@@ -13,28 +13,6 @@ namespace TrueCraft.Client.Rendering.Blocks
             BlockRenderer.RegisterRenderer(CobwebBlock.BlockID, new CobwebRenderer());
         }
 
-        protected Vector2 CobwebTextureMap { get { return new Vector2(11, 0); } }
-        protected Vector2[] CobwebTexture;
-
-        public CobwebRenderer()
-        {
-            CobwebTexture = new[]
-                {
-                    CobwebTextureMap + Vector2.UnitX + Vector2.UnitY,
-                    CobwebTextureMap + Vector2.UnitY,
-                    CobwebTextureMap,
-                    CobwebTextureMap + Vector2.UnitX,
-                };
-            for (int i = 0; i < Texture.Length; i++)
-            {
-                CobwebTexture[i] *= new Vector2(16f / 256f);
-            }
-        }
-
-        public override VertexPositionNormalColorTexture[] Render(BlockDescriptor descriptor, Vector3 offset,
-            Tuple<int, int> textureMap, int indiciesOffset, out int[] indicies)
-        {
-            return RenderQuads(descriptor, offset, CobwebTexture, indiciesOffset, out indicies, Color.White);
-        }
+        protected override Vector2 TextureMap { get { return new Vector2(11, 0); } }
     }
 }
