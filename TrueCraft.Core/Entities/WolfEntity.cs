1429fce68d0b6f3d32fbbcfe3c34693bf853de80
Drew DeVault <sir@cmpwn.com>
2015-09-07 14:34:16 -0400

    Add Wolf entity

---
+++ TrueCraft.Core/Entities/WolfEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TrueCraft.API;
+
+namespace TrueCraft.Core.Entities
+{
+    public class WolfEntity : MobEntity
+    {
+        public override Size Size
+        {
+            get
+            {
+                return new Size(0.6, 1.8, 0.6);
+            }
+        }
+
+        public override short MaxHealth
+        {
+            get
+            {
+                return 10;
+            }
+        }
+
+        public override sbyte MobType
+        {
+            get
+            {
+                return 95;
+            }
+        }
+    }
+}
