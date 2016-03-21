e8d962db48ac84258373bd45cc139e07b45663b1
Peroalane <stian.furu.overbye@gmail.com>
2016-03-21 13:24:56 +0100

    Add list command

---
+++ TrueCraft/Commands/ListCommand.cs
@@ -0,0 +1,33 @@
+﻿using TrueCraft.API.Networking;
+
+namespace TrueCraft.Commands
+{
+    public class ListCommand : Command
+    {
+        public override string Name
+        {
+            get { return "list"; }
+        }
+
+        public override string Description
+        {
+            get { return "Returns a list of all online players"; }
+        }
+
+        public override void Handle(IRemoteClient client, string alias, string[] arguments)
+        {
+            string toSend = "Current online players: ";
+            foreach (IRemoteClient i in client.Server.Clients)
+            {
+                toSend = toSend + i.Username + ", ";
+            }
+            toSend = toSend.Remove(toSend.Length - 2);
+            client.SendMessage(toSend);
+        }
+
+        public override void Help(IRemoteClient client, string alias, string[] arguments)
+        {
+            client.SendMessage("Correct usage is /" + alias);
+        }
+    }
+}
