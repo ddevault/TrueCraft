fd7c3b3a1e0910d3a777e22d6f4acbcc4763fe40
Peroalane <stian.furu.overbye@gmail.com>
2016-03-21 14:11:54 +0100

    Add safeguard

--- TrueCraft/Commands/ListCommand.cs
+++ TrueCraft/Commands/ListCommand.cs
@@ -1,4 +1,5 @@
-﻿using TrueCraft.API.Networking;
+﻿using System.Text;
+using TrueCraft.API.Networking;
 
 namespace TrueCraft.Commands
 {
@@ -11,15 +12,21 @@ namespace TrueCraft.Commands
 
         public override string Description
         {
-            get { return "Returns a list of all online players"; }
+            get { return "Lists players on the server"; }
         }
 
         public override void Handle(IRemoteClient client, string alias, string[] arguments)
         {
-            string toSend = "Current online players: ";
+            string toSend = "Currently connected players: ";
             foreach (IRemoteClient i in client.Server.Clients)
             {
                 toSend = toSend + i.Username + ", ";
+                if (ASCIIEncoding.Unicode.GetByteCount(toSend.ToCharArray) > 32767)
+                {
+                    client.SendMessage("Too many online players.");
+                    return;
+                }
+
             }
             toSend = toSend.Remove(toSend.Length - 2);
             client.SendMessage(toSend);
